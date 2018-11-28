3dc94e1eaaad6fd68dc028b624b33b115d80b2e0
Om <Om@MyMac001.local>
2018-11-28 19:50:43 +0530

    added bits

--- tomcat_start.sh
+++ tomcat_start.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.8);/Users/Om/tomcat-8.0.23/apache-tomcat-8.0.23/bin/startup.sh
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+/Users/Om/tomcat-8.0.23/apache-tomcat-8.0.23/bin/startup.sh
