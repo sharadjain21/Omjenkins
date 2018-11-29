281bd2db263218d3cce910b2324f297ead3e9a22
Om <Om@MyMac001.local>
2018-11-29 11:10:24 +0530

    changed start script2

--- tomcat_start.sh
+++ tomcat_start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+export CATALINA_HOME=/Users/Om/tomcat-8.0.23/apache-tomcat-8.0.23
 /Users/Om/tomcat-8.0.23/apache-tomcat-8.0.23/bin/startup.sh
