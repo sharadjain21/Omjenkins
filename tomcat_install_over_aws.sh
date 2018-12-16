c2a7d30d5cefe1099af432aaeca64e7299ba1c3b
Om <Om@MyMac001.local>
2018-12-16 16:07:32 +0530

    Added logic for version

--- tomcat_install_over_aws.sh
+++ tomcat_install_over_aws.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 cd /tmp/abcdcheck
-tar xvzf apache-tomcat-8.0.23.tar.gz
-mkdir -p /$HOME/tomcat-8.0.23
-cp -R apache-tomcat-8.0.23 /$HOME/tomcat-8.0.23/
+versn1 = $1
+tomcat_to_be_installed = apache-tomcat-$versn1
+#tar xvzf apache-tomcat-8.0.23.tar.gz
+#mkdir -p /$HOME/tomcat-8.0.23
+#cp -R apache-tomcat-8.0.23 /$HOME/tomcat-8.0.23/
+tar xvzf $tomcat_to_be_installed.tar.gz
+mkdir -p /$HOME/$tomcat_to_be_installed
+cp -R $tomcat_to_be_installed /$HOME/$tomcat_to_be_installed/
