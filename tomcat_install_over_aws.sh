a24e6a9a3322bf2356975b4a9115174969e140dc
Om <Om@MyMac001.local>
2018-12-18 19:07:30 +0530

    changed sub_version logic

--- tomcat_install_over_aws.sh
+++ tomcat_install_over_aws.sh
@@ -2,7 +2,8 @@
 cd /tmp/abcdcheck
 echo "Hello passed variable is $1"
 versn1=$1
-tomcat_to_be_installed=apache-tomcat-$versn1
+sversion1=$2
+tomcat_to_be_installed=apache-tomcat-$versn1$sversion1
 #tar xvzf apache-tomcat-8.0.23.tar.gz
 #mkdir -p /$HOME/tomcat-8.0.23
 #cp -R apache-tomcat-8.0.23 /$HOME/tomcat-8.0.23/
