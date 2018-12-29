8cc43f74edb349e05fbbf21501c28d1e43167f57
Om <Om@MyMac001.local>
2018-12-29 17:06:45 +0530

    added subversion thing

--- tomcat_install_over_aws.sh
+++ tomcat_install_over_aws.sh
@@ -4,6 +4,7 @@ echo "Hello passed variable is $1 and $2"
 versn1=$1
 sversion1=$2
 tomcat_to_be_installed=apache-tomcat-$versn1.$sversion1
+echo "tomcat to be installed is --> $tomcat_to_be_installed"
 #tar xvzf apache-tomcat-8.0.23.tar.gz
 #mkdir -p /$HOME/tomcat-8.0.23
 #cp -R apache-tomcat-8.0.23 /$HOME/tomcat-8.0.23/
