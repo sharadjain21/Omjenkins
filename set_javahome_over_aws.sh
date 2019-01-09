85a47d6e1bea3aad588fa12c5f74d40b2089dd65
Om <Om@MyMac001.local>
2019-01-09 19:07:03 +0530

    Added JAVA install prg2

--- set_javahome_over_aws.sh
+++ set_javahome_over_aws.sh
@@ -7,7 +7,7 @@ rm -rf *.gz
 mkdir -p ~/java_for_tomcat
 cp -R  version* ~/java_for_tomcat/
 cd ~/java_for_tomcat/version*
-java_name=`${PWD##*/}`
+java_name=${PWD##*/}
 echo "export JAVA_HOME=/java_for_tomcat/$java_name" > ~/.profile
-echo "export PATH=$JAVA_HOME/bin:$PATH" >> ~/.profile
+echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> ~/.profile
 . ~/.profile
