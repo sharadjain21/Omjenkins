a7185f56f9c3138857a5bee7cabaac770f0014ef
Om <Om@MyMac001.local>
2019-01-11 10:53:46 +0530

    changed script1

--- set_javahome_over_aws.sh
+++ set_javahome_over_aws.sh
@@ -4,8 +4,8 @@ jdk_version=$1
 echo $jdk_version > ~/.profile
 version=`echo $jdk_version | cut -d "." -f1`
 echo $version >> ~/.profile
-tar xvzf jdk_version
-#rm -rf *.gz
+tar xvzf $jdk_version
+rm -rf *.gz
 #mkdir -p ~/java_for_tomcat
 cp -R  version* ~/java_for_tomcat/
 cd ~/java_for_tomcat/version*
