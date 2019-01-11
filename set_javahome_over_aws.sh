29659d6ad6730a4e6f0b1d1ab5cd22490e363a33
Om <Om@MyMac001.local>
2019-01-11 11:11:13 +0530

    changed script4

--- set_javahome_over_aws.sh
+++ set_javahome_over_aws.sh
@@ -10,7 +10,7 @@ mkdir -p ~/java_for_tomcat
 cp -R  $version* ~/java_for_tomcat/
 cd ~/java_for_tomcat/$version*
 java_name=${PWD##*/}
-echo "Hello $java_name" >> ~/.profile
-echo "export JAVA_HOME=/java_for_tomcat/$java_name" >> ~/.profile
+#echo "Hello $java_name" >> ~/.profile
+echo "export JAVA_HOME=/java_for_tomcat/$java_name" > ~/.profile
 echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> ~/.profile
 . ~/.profile
