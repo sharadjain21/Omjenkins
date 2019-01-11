b2abc5495be2b3977b10ec0de95c33ec7556125a
Om <Om@MyMac001.local>
2019-01-11 17:42:29 +0530

    changed script6

--- tomcat_start.sh
+++ tomcat_start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.8);/Users/Om/tomcat-8.0.23/apache-tomcat-8.0.23/bin/startup.sh
+#export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+. ~/.profile
+/Users/Om/tomcat-8.0.23/apache-tomcat-8.0.23/bin/startup.sh
 touch /tmp/abcd
--- tomcat_stop.sh
+++ tomcat_stop.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.8);/Users/Om/tomcat-8.0.23/apache-tomcat-8.0.23/bin/shutdown.sh
+#export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+. ~/.profile
+/Users/Om/tomcat-8.0.23/apache-tomcat-8.0.23/bin/shutdown.sh
